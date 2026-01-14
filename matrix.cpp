0d791b109d9b43f5a98f5b2b9fc51c7975984fbb
Ashaypatakare04 <patakareashay@gmail.com>
2026-01-14 10:43:35 +0530

    matrix operations

--- matrix.cpp
+++ matrix.cpp
@@ -1,47 +1,51 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int a[10][10];
-    int b[10][10];
-    int c[10][10];
-    int d,i,j,n;
-    cout<<"enter the degree of matrix:"<<endl;
-    cin>>n;
-    cout<<"Enter the elements of  matrix1 "<<endl;
-    for(i=0;i<=n;i++){
-        for(j=0;j<=n;j++){
-            cin>>a[i][j];
-        }
-    }
-    cout<<"Enter the elements of  matrix2 "<<endl;
-    for(i=0;i<=n;i++){
-        for(j=0;j<=n;j++){
-            cin>>b[i][j];
+
+int main() {
+    int a[10][10], b[10][10], c[10][10];
+    int i, j, n, d;
+
+    cout << "Enter the order of matrix: ";
+    cin >> n;
+
+    cout << "Enter the elements of matrix 1:\n";
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            cin >> a[i][j];
         }
     }
 
-    cout<<"1.add  2.sub "<<endl;
-    cin>>d;
-    if (d==1){
-        for(i=0;i<=n;i++){
-        for(j=0;j<=n;j++){
-            c[i][j]=a[i][j]+b[i][j];
+    cout << "Enter the elements of matrix 2:\n";
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            cin >> b[i][j];
         }
     }
-    }
-    else{
-        for(i=0;i<=n;i++){
-        for(j=0;j<=n;j++){
-            c[i][j]=a[i][j]-b[i][j];
+
+    cout << "1. Add  2. Subtract\n";
+    cin >> d;
+
+    if(d == 1) {
+        for(i = 0; i < n; i++) {
+            for(j = 0; j < n; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+            }
         }
-    }
-    }
-    cout<<"Elements of matrix 3:"<<endl;
-     for(i=0;i<=n;i++){
-        for(j=0;j<=n;j++){
-            cout<<c[i][j]<<" ";
+    } else {
+        for(i = 0; i < n; i++) {
+            for(j = 0; j < n; j++) {
+                c[i][j] = a[i][j] - b[i][j];
+            }
         }
-        cout<<endl;    
-}
-return 0;
+    } 
+
+    cout << "Resultant Matrix:\n";
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            cout << c[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
